e14a0b2e7982e9dad18b010b643a84d17e76fa9d
Lujan2307 <mlujan.2325@gmail.com>
2026-02-10 11:49:48 -0500

    Introdución Variables

---
+++ Mi primera cabalgata/Assets/_Main/Scripts/Variables.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    string MyName = "Miguel Angel";
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hola " + MyName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Hola Mundo Uptade() !!");
+    }
+}
