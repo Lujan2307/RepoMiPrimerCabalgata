1a3333bcc97763624d01bb4508f3eb8fab7b0df9
Lujan2307 <mlujan.2325@gmail.com>
2026-02-17 11:25:49 -0500

    Variables 3

--- Mi primera cabalgata/AGENTS.md
+++ Mi primera cabalgata/AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: Mi primera cabalgata
 - Unity version: Unity 2023.1.0f1
 - Active game object:
-  - Name: GameObject
+  - Name: Cube (1)
   - Tag: Untagged
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Mi primera cabalgata/Assets/_Main/Scripts/Variables.cs
+++ Mi primera cabalgata/Assets/_Main/Scripts/Variables.cs
@@ -7,15 +7,24 @@ public class Variables : MonoBehaviour
 
     public string myName = "Miguel Angel";
     public int edad = 19;
+    public bool isBool;
 
     [SerializeField] private TMP_InputField _inputfield;
     [SerializeField] private TMP_Text _saludo;
+    [SerializeField] private Renderer _cubo;
+    [SerializeField] private BoxCollider _BoxCollider;
 
 
     // Start is called before the first frame update
     void Start()
     {
-   
+        _cubo.material.color = Color.blue;
+        _BoxCollider.isTrigger = false;
+
+
+
+        myName = _inputfield.text;
+        _saludo.text = "Hola " + myName;
     }
 
     // Update is called once per frame
